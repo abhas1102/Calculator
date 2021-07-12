e5fcde6be1dfa23916dc9bbf4e03116733c541c8
Abhas Kumar <abhas1102@gmail.com>
2021-07-12 13:59:40 +0530

    1. Implemented onClick on each button. 2. Appended the text of button to result textview on clicking by casting the view as button. 3. Implemented Clear button functionality

--- app/src/main/java/com/example/calculatorapplication/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.calculatorapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+        calculation_view.append((view as Button).text)
+    }
+    fun onClear(view: View){
+        calculation_view.setText("")
     }
 }
